a631050cd701a799e33ef8ca0015d9b9606440eb
Victor Morais <moraisinho7@gmail.com>
2021-07-15 15:22:13 -0300

    adicionado forms.js e organizado código

---
+++ js/calcula-imc.js
@@ -0,0 +1,45 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+
+for(var i = 0; i < pacientes.length; i++){
+    var paciente = pacientes[i];
+var tdAltura = paciente.querySelector(".info-altura");
+var tdPeso = paciente.querySelector(".info-peso");
+var tdImc = paciente.querySelector(".info-imc");
+
+
+var altura = tdAltura.textContent;
+var peso = tdPeso.textContent;
+
+var alturaEhValida = true;
+var pesoEhValido = true;
+
+if (peso <= 0 || peso > 1000) {
+    console.log("Peso inválido!");
+    tdPeso.textContent = "Peso inválido!";
+    pesoEhValido = false;
+    paciente.classList.add("paciente-invalido");
+
+}
+
+if (altura <= 0 || altura >= 3) {
+    console.log("Altura inválida!");
+    tdAltura.textContent = "Altura inválida!";
+    alturaEhValida = false;
+    paciente.classList.add("paciente-invalido");
+}
+
+if (alturaEhValida && pesoEhValido) {
+
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);    
+} else {
+    tdImc.textContent = "Altura e/ou peso inválidos!"
+}
+}
+
+
+
+
+
+
